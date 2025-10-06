e630e87be01f8198cb1b422dcbf79e1753eb7354
Daisy Boxall <daisy.boxall@student.manchester.ac.uk>
2025-10-06 14:35:21 +0100

    week1/out/1.png

--- week1/week1.py
+++ week1/week1.py
@@ -7,6 +7,8 @@ print (the_perfect_variable)
 from geopandas import read_file #used to get into a GeoDataFrame, makes functionality from libraries, no import first means code will have no idea.
 
 world = read_file("../../data/natural-earth/ne_50m_admin_0_countries.shp") #imported from geopandas library, new variable called world
+graticule = read_file("../../data/natural-earth/ne_110m_graticules_15.shp")
+bbox = read_file("../../data/natural-earth/ne_110m_wgs84_bounding_box.shp")
 
 print(world.head()) #convinience function that returns the first 5 rows of the dataset aka the head. therefore prints head 
 
@@ -14,8 +16,26 @@ from matplotlib.pyplot import subplots, savefig
 # create map axis object
 my_fig, my_ax = subplots(1, 1, figsize=(16, 10))
 
-# plot the countries onto ax
-world.plot(ax = my_ax)
+# add bounding box and graticule layers
+bbox.plot(
+    ax = my_ax,
+    color = 'lightgray',
+    linewidth = 0,
+    )
+
+# plot the countries
+world.plot(
+    ax = my_ax,
+    color = 'black',
+    linewidth = 0.5,
+    )
+
+# plot the graticule
+graticule.plot(
+    ax = my_ax,
+    color = 'white',
+    linewidth = 0.5,
+    )
 # turn off the visible axes on the map
 my_ax.axis('off')
 # save the result
