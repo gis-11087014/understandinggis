999ec39258ceed2b7c773cc83345cb0f0a2cea45
Daisy Boxall <daisy.boxall@student.manchester.ac.uk>
2025-10-06 15:00:09 +0100

    week1/out/1.png

--- week1/week1.py
+++ week1/week1.py
@@ -10,8 +10,17 @@ world = read_file("../../data/natural-earth/ne_50m_admin_0_countries.shp") #impo
 print(world.columns)
 graticule = read_file("../../data/natural-earth/ne_110m_graticules_15.shp")
 bbox = read_file("../../data/natural-earth/ne_110m_wgs84_bounding_box.shp")
+ea_proj = ("+proj=eqearth +lon_0=0 +datum=WGS84 +units=m +no_defs")
+
+# reproject all three layers to equal earth
+world = world.to_crs(ea_proj)
+graticule = graticule.to_crs(ea_proj)
+bbox = bbox.to_crs(ea_proj)
 
 print(world.head()) #convinience function that returns the first 5 rows of the dataset aka the head. therefore prints head 
+
+world['pop_density'] = world['POP_EST'] / (world.area / 1000000)
+print(world.columns)
 
 from matplotlib.pyplot import subplots, savefig
 # create map axis object
@@ -27,11 +36,16 @@ bbox.plot(
 # plot the countries
 world.plot(								# plot the world dataset
     ax = my_ax,						# specify the axis object to draw it to
-    column = 'POP_EST',		# specify the column used to style the dataset
+    column = 'pop_density',		# specify the column used to style the dataset
     cmap = 'YlGnBu',				# specify the colour map used to style the dataset based on POP_EST
     scheme = 'quantiles',	# specify how the colour map will be mapped to the values in POP_EST
     linewidth = 0.5,			# specify the line width for the country outlines
     edgecolor = 'gray',		# specify the line colour for the country outlines
+    legend = 'True',
+legend_kwds = {
+    'loc': 'lower left',
+    'title': 'Population Density'
+    }
     )
 
 # plot the graticule
@@ -42,6 +56,9 @@ graticule.plot(
     )
 # turn off the visible axes on the map
 my_ax.axis('off')
+
+my_ax.set(title="Population Density: Equal Earth Coordinate Reference System")
+
 # save the result
 savefig('week1/out/1.png')
 print("done!")
