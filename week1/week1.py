35abcec3d213fd1ecbe508db43b1b7cba232c68b
Daisy Boxall <daisy.boxall@student.manchester.ac.uk>
2025-10-06 14:43:51 +0100

    week1/out/1.png

--- week1/week1.py
+++ week1/week1.py
@@ -20,15 +20,18 @@ my_fig, my_ax = subplots(1, 1, figsize=(16, 10))
 # add bounding box and graticule layers
 bbox.plot(
     ax = my_ax,
-    color = 'skyblue',
+    color = 'lightblue',
     linewidth = 0,
     )
 
 # plot the countries
-world.plot(
-    ax = my_ax,
-    color = 'darkgreen',
-    linewidth = 0.5,
+world.plot(								# plot the world dataset
+    ax = my_ax,						# specify the axis object to draw it to
+    column = 'POP_EST',		# specify the column used to style the dataset
+    cmap = 'YlGnBu',				# specify the colour map used to style the dataset based on POP_EST
+    scheme = 'quantiles',	# specify how the colour map will be mapped to the values in POP_EST
+    linewidth = 0.5,			# specify the line width for the country outlines
+    edgecolor = 'gray',		# specify the line colour for the country outlines
     )
 
 # plot the graticule
