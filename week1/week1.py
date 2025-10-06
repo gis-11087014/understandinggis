31cbe37cf38c8953a9c65f5e5293182460774ef9
Daisy Boxall <daisy.boxall@student.manchester.ac.uk>
2025-10-06 14:16:05 +0100

    load world shapefile as GeoDataFrame

--- week1/week1.py
+++ week1/week1.py
@@ -1,2 +1,11 @@
-print("hello world!")
-print (2+2)
+big_fat_variable = "twat"
+little_bitch_variable = "cunt"
+the_perfect_variable = big_fat_variable + little_bitch_variable
+print (the_perfect_variable)
+
+from geopandas import read_file
+
+world = read_file("../../data/natural-earth/ne_50m_admin_0_countries.shp")
+
+print(world.head())
+
