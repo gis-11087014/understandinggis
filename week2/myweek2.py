2f0e8439673e16a5ff521dc47300286500bddb6d
gis-11087014 <daisy.boxall@student.manchester.ac.uk>
2025-10-08 14:23:38 +0100

    Wall Map Made

--- week2/myweek2.py
+++ week2/myweek2.py
@@ -12,6 +12,9 @@ from pyproj import Geod
 # load the shapefile of countries - this gives a table of 12 columns and 246 rows (one per country)
 world = read_file("../../data/natural-earth/ne_10m_admin_0_countries.shp")
 
+ #open the graticule dataset
+graticule = read_file("../../data/natural-earth/ne_110m_graticules_5.shp")
+
 # print a list of all of the columns in the shapefile
 #a simple list of all the columns avaliable 
 print(world.columns)
@@ -60,19 +63,6 @@ border = usa_geom.intersection(mex_geom)
 
 print(border)
 
-# create map axis object
-my_fig, my_ax = subplots(1, 1, figsize=(16, 10))
-
-# remove axes
-my_ax.axis('off')
-
-# plot the border
-GeoSeries(border).plot(
-  ax = my_ax
-	)
-
-# save the image
-savefig('./out/first-border.png')
 
 #use ellipsodial distance claculations to avoid erros when using projections too big 
 #pyproj is the most accurate as it does the complex maths invloved in this projetcion
@@ -132,5 +122,56 @@ for segment in border.geoms:
 #initialise startign value 0 as cum length, then a loop through the list, then addign the distcances with += to 0 
 
 print(cumulative_length)
+
+lambert_conic = ("+proj=lcc +lon_0=-106.3476562 +lat_1=22.645838 +lat_2=37.3130861 +lat_0=29.979462 +datum=WGS84 +units=m +no_defs")
+#this is the porjection from the wizard website made into a varible 
+
+# create map axis object
+my_fig, my_ax = subplots(1, 1, figsize=(16, 10))
+
+# remove axes
+my_ax.axis('off')
+
+# set title
+title(f"Trump's wall would have been {cumulative_length / 1000:.2f} km long.")
+
+# project border
+border_series = GeoSeries(border, crs=world.crs).to_crs(lambert_conic)
+
+# extract the bounds from the (projected) GeoSeries Object
+minx, miny, maxx, maxy = border_series.geometry.iloc[0].bounds
+
+# set bounds (10000m buffer around the border itself, to give us some context)
+buffer = 10000
+my_ax.set_xlim([minx - buffer, maxx + buffer])
+my_ax.set_ylim([miny - buffer, maxy + buffer])
+
+# plot data
+usa.to_crs(lambert_conic).plot(
+    ax = my_ax,
+    color = '#ccebc5',
+    edgecolor = '#4daf4a',
+    linewidth = 0.5,
+    )
+mex.to_crs(lambert_conic).plot(
+    ax = my_ax,
+    color = '#fed9a6',
+    edgecolor = '#ff7f00',
+    linewidth = 0.5,
+    )
+border_series.plot(     # note that this has already been projected above!
+    ax = my_ax,
+    color = '#984ea3',
+    linewidth = 2,
+    )
+graticule.to_crs(lambert_conic).plot(
+    ax=my_ax,
+    color='grey',
+    linewidth = 1,
+    )
+
+# save the result
+savefig('out/2.png', bbox_inches='tight')
+print("done!")
 
 
