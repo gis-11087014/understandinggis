7f520c61a9700e012aa668df61a173e137df5e5b
gis-11087014 <daisy.boxall@student.manchester.ac.uk>
2025-10-08 14:05:12 +0100

    Update myweek2.py

--- week2/myweek2.py
+++ week2/myweek2.py
@@ -7,6 +7,7 @@ Created on Tue Oct  7 12:47:55 2025
 #geopandas uses the object GeoDataFrame to hold the data and the geometry in the dataset
 from geopandas import read_file, GeoSeries
 from matplotlib.pyplot import subplots, savefig, title
+from pyproj import Geod
 
 # load the shapefile of countries - this gives a table of 12 columns and 246 rows (one per country)
 world = read_file("../../data/natural-earth/ne_10m_admin_0_countries.shp")
@@ -72,3 +73,63 @@ GeoSeries(border).plot(
 
 # save the image
 savefig('./out/first-border.png')
+
+#use ellipsodial distance claculations to avoid erros when using projections too big 
+#pyproj is the most accurate as it does the complex maths invloved in this projetcion
+
+# set which ellipsoid you would like to use
+#creates a Geod object based on WGS84 - common general model of the shape of the earth 
+#stored in variable g 
+#gives access to vincenty equations (pair of equations) for ellipsoid measurements to be made 
+#forward and inverse equations - Geod.fwd and Geod.inv 
+#fwd calcs point on sphere a given distance and direction away from another - locates and finds the point based on a distance
+#inv calcs the distance and direction between a pair of points - calculates the distacne 
+#cuz earth is round, the equations calc the distored striaght line that actually follows the earth - liek a plane 
+#to do this inv returns 2 directions (fwd azimuth - diuirection from point A u set off from - and back azimuth - direstion set of from B to A) 
+g = Geod(ellps='WGS84')
+
+#g.inv function makes a list of 3 values which is made into variables by python 
+#we dont want these - it makes azF and azB and rthen the result, we only want the reuslt 
+# so we use a code that only give us the 3rd value which is teh actual reuslt. 
+#basically choosing not to store them cuz they useless
+#to calc a distance we need to put in 4 arguments for the function to run - the long and lat of both points
+
+print(border)
+
+#multilinestign is made up of linestring (segemnts) where the end point of one is the start of another 
+#to work out length, we need to work out the lengeth of each segment and add the together 
+#so we make a loop - measure the distances and add them together to make a total 
+#to make loop, convert it to a list of its constituent LineString using .geoms property 
+#this gives access to the start and end of the coords of each segemtn usign segment.coords[0] and segemnt.coords[1] and so on 
+
+# loop through each segment in the line and print the coordinates
+for segment in border.geoms:
+	print(f"from:{segment.coords[0]}\tto:{segment.coords[1]}")
+
+#the /t makes it a neat list 
+#now the pairs of coords are stores as a tuple (list but unchangebale)
+#to access a single tuple, use [] to specify the index value 
+#but as its a list of tuples we need to use 2 index references (the []), one to reference coord pair (start n end) and one to ref the coord (llong n lat) 
+# cux long before lat the long value from the start point (sengment.coords[0])woudl be segmemnt segment.coords[0][0] whereas the lat from the end point would be [1] etc 
+#(segment.coords[0] is the start point so putting another [0] makes it longitute and [1] is the end or the lat )
+
+# initialise a variable to hold the cumulative length
+cumulative_length = 0
+
+# loop through each segment in the line
+
+for segment in border.geoms:
+    print(f"from:{segment.coords[0]}\tto:{segment.coords[1]}")
+    
+	# THIS LINE NEEDS COMPLETING
+    # segment.coords[0] is the start point and [1] is the end so putting a second [0] on the end makes it long cuz that was first and then addign a [1] is lat 
+    distance = g.inv(segment.coords[0][0], segment.coords[0][1], segment.coords[1][0], segment.coords[1][1])[2]
+
+	# add the distance to our cumulative total
+    cumulative_length += distance
+
+#initialise startign value 0 as cum length, then a loop through the list, then addign the distcances with += to 0 
+
+print(cumulative_length)
+
+
