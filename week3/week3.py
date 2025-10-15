b2db5b0e131343d33aae5d33b13ff5eda38d8500
gis-11087014 <daisy.boxall@student.manchester.ac.uk>
2025-10-15 17:16:03 +0100

    Printing EPSG Values

--- week3/week3.py
+++ week3/week3.py
@@ -30,9 +30,33 @@ def distance(x1, y1, x2, y2):
 	"""
 	return sqrt((x2-x1)**2 + (y2-y1)**2)
 # complete this line to return the distance between (x1,y1) and (x2,y2)
+#** is sqaure in code
 
 result = distance (345678, 456789 , 445678, 556789)
+#result is the end of the code and using ur specificvalues 
 
-print(result)
+print(result) 
 
-#a2 + b2 = c2 
+#BORE HOLES 
+#will calc mean walking distance to holes 
+#assumptions made cuz map data scarce here i.e. as the crow flies; people location doen with pop model on facebook labs; using already exsitsing boreholes even thoguh incom plete 
+#use spatial index to make calcs as efficient as possible
+#we will make our own spatial index with STRtree class in the shapley library. 
+
+#need to use CRS so no distortiosn occur so must make sure all data is in same CRS and appropriate for Northen Uganada
+#geopandas makes this easy from getting GDF back as an object so we can use .crs to check the CRS and .to_crs() to change it if needed
+
+from geopandas import read_file
+
+# read in shapefiles, ensure that they all have the same CRS
+pop_points = read_file("../../data/gulu/pop_points.shp")
+water_points = read_file("../../data/gulu/water_points.shp")
+gulu_district = read_file("../../data/gulu/district.shp")
+
+#cuz three main CRS types, they could come out as any which is bad and hard to compare 
+#but we can specifiy yay! using member functions 
+#to use, just add on the end of .crs 
+
+print(pop_points.crs.to_epsg()) 
+print(water_points.crs.to_epsg()) 
+print(gulu_district.crs.to_epsg()) 
