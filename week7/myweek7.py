255933b89693327f8f7e77a98a756cb9d838ff4a
gis-11087014 <daisy.boxall@student.manchester.ac.uk>
2025-11-11 12:43:20 +0000

    Creating Empty Layer

--- week7/myweek7.py
+++ week7/myweek7.py
@@ -2,6 +2,8 @@ from rasterio import open as rio_open
 from rasterio.transform import rowcol
 from rasterio.plot import show as rio_show
 from matplotlib.pyplot import subplots, savefig
+from numpy import zeros
+
 
 def coord_2_img(transform, x, y):
 	""" 
@@ -17,6 +19,10 @@ with rio_open("../../data/helvellyn/Helvellyn-50.tif") as dem:
     # read the data out of band 1 in the dataset
     dem_data = dem.read(1)
     
+    #zero() creates a list of 0s, makes a numpy array 2D dataset 
+    # create a new 'band' of raster data the same size
+    output = zeros(dem_data.shape)
+    
     # plot the dataset
 fig, my_ax = subplots(1, 1, figsize=(16, 10))
 
@@ -26,6 +32,14 @@ rio_show(
   ax=my_ax,
   transform = dem.transform,
 )
+
+#plotting empty 0s ontop of the DEM 
+# add the empty layer
+rio_show(
+    output,
+    ax=my_ax,
+    transform=dem.transform
+    )
 
 # save the resulting map
 savefig('./out/6.png', bbox_inches='tight')
