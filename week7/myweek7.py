0d19d79e05f0ca475603dd729de482e6ff8ab19e
gis-11087014 <daisy.boxall@student.manchester.ac.uk>
2025-11-11 12:47:18 +0000

    Invisible Layer

--- week7/myweek7.py
+++ week7/myweek7.py
@@ -3,7 +3,7 @@ from rasterio.transform import rowcol
 from rasterio.plot import show as rio_show
 from matplotlib.pyplot import subplots, savefig
 from numpy import zeros
-
+from matplotlib.colors import LinearSegmentedColormap
 
 def coord_2_img(transform, x, y):
 	""" 
@@ -33,14 +33,22 @@ rio_show(
   transform = dem.transform,
 )
 
-#plotting empty 0s ontop of the DEM 
-# add the empty layer
+# add the empty layer, plotting empty 0s ontop of the DEM 
 rio_show(
     output,
     ax=my_ax,
-    transform=dem.transform
+    transform=dem.transform,
+    cmap = LinearSegmentedColormap.from_list('binary', [(0, 0, 0, 0), (0, 0.5, 1, 0.5)], N=2)
     )
+
+#CMAP EXPLAINAITON 
+#now edit colour map of layer so it doesnt obscure DEM by cretaing classified colout scheme
+#add a list of tuples where 0=invisible and 1=blue (where we want new info), a scale of 0-1
+#goes it goes red, green, blue, alpha to make colours 
 
 # save the resulting map
 savefig('./out/6.png', bbox_inches='tight')
-    
+
+
+#now edit colour map of layer so it doesnt obscure DEM by cretaing classified colout scheme
+#add a list of tuples where 0=invisible and 1=blue (where we want new info), a scale of 0-1
