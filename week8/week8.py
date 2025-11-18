6dbcdaab14afab0e03f40dca697110b87d257b34
gis-11087014 <daisy.boxall@student.manchester.ac.uk>
2025-11-18 13:12:02 +0000

    More Bullshit

--- week8/week8.py
+++ week8/week8.py
@@ -2,6 +2,9 @@ from rasterio import open as rio_open
 from rasterio.plot import show as rio_show
 from rasterio.transform import rowcol
 from sys import exit
+from numpy import zeros
+from numpy import column_stack
+from skimage.draw import circle_perimeter
 
 # set origin for viewshed
 x0, y0 = 330000, 512500
@@ -39,10 +42,19 @@ def viewshed(x0, y0, radius_m, observer_height, traget_height, dem_data, transfo
     #height means elevation of origin location PLUS the obsever height 
     #so read DEM and add it to obsever height
     # get the observer height (above sea level)
+    #remeber [] not () so it knows it numerical !!!
     height0 = dem_data[r0, c0] + observer_height
-    
     print(height0)
     
+    # get pixels in the perimeter of the viewshed
+    for r, c in column_stack(circle_perimeter(r0, c0, radius_px)):
+
+	# calculate line of sight to each pixel, pass output and get a new one back each time
+	#output = line_of_sight(r0, c0, height0, r, c, target_height, radius_px, dem_data, transform, output)
+        pass
+
+    # return the resulting viewshed
+    return output
     
 def line_of_sight():
     """
@@ -55,5 +67,21 @@ with rio_open("../../data/helvellyn/Helvellyn-50.tif") as dem:
     # read the data out of band 1 in the dataset
     dem_data = dem.read(1)
     
+    #transform into image space
+    r0, c0 = coord_2_img(dem.trasnform, x0, y0)
+    
+    output = zeros(dem_data.shape)
+
+    dem_data[dem.index(r0, c0)] = 1
+    
     # calculate the viewshed
     output = viewshed(x0, y0, 20000, 1.8, 100, dem_data, dem.transform)
+    
+    
+    
+    
+    
+    
+    
+    
+    
