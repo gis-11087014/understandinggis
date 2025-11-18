e7d41c46f0c6e9ffaa91c36f7301c725ed05535c
gis-11087014 <daisy.boxall@student.manchester.ac.uk>
2025-11-18 12:46:20 +0000

    Veiwshed function

--- week8/week8.py
+++ week8/week8.py
@@ -1,8 +1,53 @@
 from rasterio import open as rio_open
 from rasterio.plot import show as rio_show
+from rasterio.transform import rowcol
+from sys import exit
 
 # set origin for viewshed
 x0, y0 = 330000, 512500
+
+#convert x0, y0 from coorf to image space
+def coord_2_img(transform, x, y):
+	""" 
+	* Convert from coordinate space to image space using the 
+	*   Affine transform object from a rasterio dataset
+	"""
+	r, c = rowcol(transform, x, y)
+	return int(r), int(c)
+                    
+#def = function
+def viewshed(x0, y0, radius_m, observer_height, traget_height, dem_data, transform):
+    """
+    works out endpoint for each line of sight (each cell in circle)
+    """
+    
+    # convert from coordinate space to image space
+    r0, c0 = rowcol(transform, x0, y0)
+    
+    #returning the height and width of the dataset with .shape and exits if the origin are largest that 0 and smaller than width/hight of dataset
+    # make sure that we are within the dataset
+    if not 0 <= r0 < dem_data.shape[0] or not 0 <= c0 < dem_data.shape[1]:
+        print(f"Sorry, {x0, y0} is not within the elevation dataset.")
+        exit()
+        
+    #WE NEED TO KNOW LENGTH OF CELL IRL 
+    #can access resolution of dataset by storign in transform[0] which takes the value of x or y as they are both the same
+    # convert the radius (m) to pixels
+    radius_px = int(radius_m / transform[0])
+    
+    #we now know starting location and radius so now need to find starting height 
+    #height means elevation of origin location PLUS the obsever height 
+    #so read DEM and add it to obsever height
+    # get the observer height (above sea level)
+    height0 = dem_data[r0, c0] + observer_height
+    
+    print(height0)
+    
+    
+def line_of_sight():
+    """
+    works out the visibility of each poitn on a line from the origin of that locaiton
+    """
 
 # open the elevation data file
 with rio_open("../../data/helvellyn/Helvellyn-50.tif") as dem:
