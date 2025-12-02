9817daa72150e4a50bc46ef350960ef1d2716afa
gis-11087014 <daisy.boxall@student.manchester.ac.uk>
2025-12-02 11:28:34 +0000

    W8

--- week8/week8.py
+++ week8/week8.py
@@ -6,6 +6,16 @@ from numpy import zeros
 from numpy import column_stack
 from skimage.draw import circle_perimeter, line
 from math import hypot
+from math import floor, ceil
+from geopandas import GeoSeries
+from shapely.geometry import Point
+from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
+from matplotlib.colors import Normalize
+from matplotlib.cm import ScalarMappable
+from matplotlib.pyplot import subplots, savefig
+from matplotlib_scalebar.scalebar import ScaleBar
+from matplotlib.colors import LinearSegmentedColormap
 
 # set origin for viewshed
 x0, y0 = 330000, 512500
@@ -20,13 +30,17 @@ def coord_2_img(transform, x, y):
     return int(r), int(c)
                     
 #def = function
-def viewshed(x0, y0, radius_m, observer_height, traget_height, dem_data, transform):
+def viewshed(x0, y0, radius_m, observer_height, target_height, dem_data, transform):
     """
     works out endpoint for each line of sight (each cell in circle)
     """
     
     # convert from coordinate space to image space
-    r0, c0 = rowcol(transform, x0, y0)
+    r0, c0 = coord_2_img(transform, x0, y0)
+    
+    output = zeros(dem_data.shape)#
+    
+    output[r0, c0] = 1
     
     #returning the height and width of the dataset with .shape and exits if the origin are largest that 0 and smaller than width/hight of dataset
     # make sure that we are within the dataset
@@ -45,15 +59,13 @@ def viewshed(x0, y0, radius_m, observer_height, traget_height, dem_data, transfo
     # get the observer height (above sea level)
     #remeber [] not () so it knows it numerical !!!
     height0 = dem_data[r0, c0] + observer_height
-    print(height0)
     
     # get pixels in the perimeter of the viewshed
     for r, c in column_stack(circle_perimeter(r0, c0, radius_px)):
 
     # calculate line of sight to each pixel, pass output and get a new one back each time
-    #output = line_of_sight(r0, c0, height0, r, c, target_height, radius_px, dem_data, transform, output)
-        pass
-
+        output = line_of_sight(r0, c0, height0, r, c, target_height, radius_px, dem_data, transform, output)
+        
     # return the resulting viewshed
     return output
     
@@ -67,7 +79,7 @@ def line_of_sight(r0, c0, height0, r1, c1, height1, radius, dem_data, transform,
     # loop along the pixels in the line (excluding the first one)
     for r, c in column_stack(line(r0, c0, r1, c1))[1:]:
         
-        dx = hypot(r - r0, c- c0)
+        dx = hypot(r - r0, c - c0)
         
         # if we go too far, or go off the edge of the data, stop looping
         if dx > radius or not 0 <= r < dem_data.shape[0] or not 0 <= c < dem_data.shape[1]:
@@ -103,7 +115,66 @@ with rio_open("../../data/helvellyn/Helvellyn-50.tif") as dem:
     # calculate the viewshed
     output = viewshed(x0, y0, 20000, 1.8, 100, dem_data, dem.transform)
     
-    
+# output image
+fig, my_ax = subplots(1, 1, figsize=(16, 10))
+my_ax.set_title("Viewshed Analysis")
+
+    # draw dem
+rio_show(
+        dem_data,
+        ax=my_ax,
+        transform = dem.transform,
+        cmap = 'viridis',
+        )
+
+    # draw dem as contours
+rio_show(
+        dem_data,
+        ax=my_ax,
+        contour=True,
+        transform = dem.transform,
+        colors = ['white'],
+        linewidths = [0.5],
+        )
+
+    # add viewshed
+rio_show(
+        output,
+        ax=my_ax,
+        transform=dem.transform,
+        cmap = LinearSegmentedColormap.from_list('binary_viewshed', [(0, 0, 0, 0), (1, 0, 0, 0.5)], N=2)
+        )
+
+    # add origin point
+GeoSeries(Point(x0, y0)).plot(
+        ax = my_ax,
+        markersize = 60,
+        color = 'black',
+        edgecolor = 'white'
+        )
+
+    # add a colour bar
+fig.colorbar(ScalarMappable(norm=Normalize(vmin=floor(dem_data.min()), vmax=ceil(dem_data.max())), cmap='viridis'), ax=my_ax, pad=0.01)
+
+    # add north arrow
+x, y, arrow_length = 0.97, 0.99, 0.1
+my_ax.annotate('N', xy=(x, y), xytext=(x, y-arrow_length),
+arrowprops=dict(facecolor='black', width=5, headwidth=15),
+ha='center', va='center', fontsize=20, xycoords=my_ax.transAxes)
+
+    # add scalebar
+my_ax.add_artist(ScaleBar(dx=1, units="m", location="lower right"))
+
+    # add legend for point
+my_ax.legend(
+        handles=[
+            Patch(facecolor=(1, 0, 0, 0.5), edgecolor=None, label=f'Visible Area'),
+            Line2D([0], [0], marker='o', color=(1,1,1,0), label='Viewshed Origin', markerfacecolor='black', markersize=8)
+        ], loc='lower left')
+
+    # save the result
+savefig('./out/7.png', bbox_inches='tight')
+print("done!")
     
     
     
