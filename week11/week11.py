441142977314960dc9bb098a142f6802b8a6ad17
gis-11087014 <daisy.boxall@student.manchester.ac.uk>
2025-12-09 12:26:43 +0000

    Found empty and Remining Houses

--- week11/week11.py
+++ week11/week11.py
@@ -20,23 +20,34 @@ class Schelling:
     * the setup of an instance of this class (i.e. 'construct' it)
     """
     
-    def __init__(self, width, height, empty_ration, similarity_threshold, n_iterations, agents): 
+    def __init__(self, width, height, empty_ratio, similarity_threshold, n_iterations, agents): 
         #storing each argument except self in instance variable with the same name
         self.width = width
         self.height = height
-        self.empty_ration = empty_ration
+        self.empty_ratio = empty_ratio
         self.similarity_threshold = similarity_threshold
         self.n_iterations = n_iterations
         self.agents = ({})
-
+        
         # get all house addresses
         all_houses = [(x, y) for x in range(self.width) for y in range(self.height)]
         
-        # shuffle the order of the houses
-        #randomises it
+        # shuffle the order of the houses, randmoises it
         shuffle(all_houses)
         
-        print (all_houses)
+        #calculating empty houses and storing as n_empty 
+        #need self and len to make them multipliable numbers   
+        n_empty = int(self.empty_ratio * len(all_houses))
+        
+        # identify the empty houses with list slicing
+        self.empty_houses = all_houses[:n_empty]
+        
+        #identify the remaining houses w/ list slicing       
+        self.remaining_houses = all_houses[n_empty:]
+        
+        #print (all_houses)
+        #print (self.empty_houses)
+        print(len(self.remaining_houses))
     
 #an instance of Schelling
 schelling = Schelling(25, 25, 0.25, 0.6, 500, ({}))
